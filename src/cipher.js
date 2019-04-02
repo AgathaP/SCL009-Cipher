b8ddf7a1b3675ea1ae360c6f701baf57b94888db
AgathaP <ebrumaktub@gmx.com>
2019-04-01 22:33:38 -0300

    función que codifica minúsculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,26 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
+
+  // Función de cifrado
+  encode: (num, text) => {
+    // convertir letra a ascii y luego aplicar formula
+    let result='';
+    let asciiText='';
+for(let i = 0; i < text.length; i++){
+  asciiText = text.charCodeAt(i);
+      // cifrado con mayúscula.
+  if(asciiText >= 65 && asciiText <= 90){
+     asciiText = String.fromCharCode((asciiText - 65 + (parseInt(num))) % 26 + 65);
+      }
+      // Cifrado con minúsculas.
+  if(asciiText >= 97 && asciiText <= 122) {
+     asciiText = String.fromCharCode((asciiText - 97 + (parseInt(num))) % 26 + 97);
+      }
+      result += asciiText
+    }  
+    return result;
   },
+
+  //Función descifra un texto 
   decode: () => {
     /* Acá va tu código */
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+// import { createCipher } from "crypto";
+
 /* Llamamos un evento para que el botón ver nos imprima en la pantalla
      lo que se ingresa en el input type password*/
 
 document.getElementById('see').addEventListener('click', () => {
     let vewPass = document.getElementById('userText').value;
     document.getElementById('yourPass').innerHTML = `${vewPass}`;
+})
+
+document.getElementById('btnConvert').addEventListener('click', () => {
+    let numUser = document.getElementById('offset').value;
+    let textUser = document.getElementById('userText').value;
+    let resultEncode = cipher.encode(numUser, textUser);
+        document.getElementById('result').innerHTML = `${resultEncode}`;
 })
