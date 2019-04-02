0d0aac43c1b60a2e03d0c399212c48924f1338e2
AgathaP <ebrumaktub@gmx.com>
2019-04-02 12:41:42 -0300

    Agrego comentarios a cada llamado de evento.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@
 
 /* Llamamos un evento para que el botón ver nos imprima en la pantalla
      lo que se ingresa en el input type password*/
-
 document.getElementById('see').addEventListener('click', () => {
     let vewPass = document.getElementById('user_text').value;
     document.getElementById('yourPass').innerHTML = `${vewPass}`;
 })
 
+/* Evento que imprime en la pantalla eltexto cifrado, 
+     mediante el botón "btn_encode"*/
 document.getElementById('btn_encode').addEventListener('click', () => {
     let numEnc = document.getElementById('offset').value;
     let textEnc = document.getElementById('user_text').value;
@@ -15,6 +16,8 @@ document.getElementById('btn_encode').addEventListener('click', () => {
         document.getElementById('result').innerHTML = `${resultEncode}`;
 })
 
+/* Evento que imprime en la pantalla eltexto descifrado, 
+     mediante el botón "btn_decode"*/
 document.getElementById('btn_decode').addEventListener('click', () => {
     let numDec = document.getElementById('offset').value;
     let textDec = document.getElementById('user_text').value;
